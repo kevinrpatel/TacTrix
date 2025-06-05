d9017a5e7296884e5420ee27a9a32d1365d4da98
Kevin Patel <Kevinr.patel26@gmail.com>
2025-06-05 14:53:56 +0530

    Update Socket.io CORS origin to deployed frontend URL

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // React app port
+   // origin: "http://localhost:5173", // React app port
+    origin: "https://tactrix.vercel.app", 
     methods: ["GET", "POST"],
     credentials: true
   }

6bf2b96b5916e93a71351aabd5621328a434c1ea
Kevin Patel <Kevinr.patel26@gmail.com>
2025-06-06 16:04:48 +0530

    update connection file

--- apis/connection.php
+++ apis/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("sql210.infinityfree.com","if0_39164654","O7c5LBenp3ulA0","if0_39164654_tactrix");
+$conn = new mysqli("localhost","root","","tic tac toe");
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
